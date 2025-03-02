2aac613d774cb7036ec5c93caf1c4382baf73854
Saket Sagar <saketsagarsingh38@gmail.com>
2025-03-02 14:49:33 +0530

    1st Lecture

---
+++ Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    //variables
+    int rollNo;
+    int age;
+    String name;
+    String address;
+    int standard;
+
+    Student(){
+        System.out.println("Constructor envoked");
+    }
+    //constructor
+    Student(int rollNo, int age, String name , String address, int standard){
+        this.rollNo = rollNo;
+        this.age = age;
+        this.name = name;
+        this.address = address;
+        this.standard = standard;
+    }
+
+    //method
+
+    @Override
+    public String toString() {
+        return "Student [rollNo=" + rollNo + ", age=" + age + ", name=" + name + ", address=" + address + ", standard="
+                + standard + "]";
+    }
+    
+
+    public static void main(String[] nikki){
+        Student s1 = new Student();
+        Student obj = new Student(10, 18, "Saket" , "Kolkata", 10);
+        System.out.println(obj.toString());
+
+        Student obj1 = new Student(11, 18, "Sagar", "Kolkata", 10);
+        System.out.println(obj1.toString());
+    }
+}
---
+++ Test.java
@@ -0,0 +1,26 @@
+public class Test{
+    //variables
+    //methods
+    //constructor
+
+    //variables
+    int a = 0;
+    boolean b = true;
+    boolean f = false;
+    float c = 0.00f;
+    double d = 9.000;
+    char p = 't';
+
+    //methods
+    public int sum(int n1, int n2){
+        return n1 + n2;
+    }
+  
+
+    public static void main(String[] args){
+        for(int i = 0 ; i < 5; i++){
+            System.out.println("Hello Nikki");
+        }
+    }
+}
+
